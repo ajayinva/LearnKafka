d2e7eb041c5d732eb07dad4d0e0a162135c87afe
Ajay A <ajayinva@gmail.com>
2017-07-03 00:10:25 -0400

    Initial Commit

---
+++ src/main/java/com/awsaces/learn/kafka/LearnKafka01.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.awsaces.learn.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * @author aagarwal
+ *
+ */
+public class LearnKafka01 {
+	public static void main(String[] args) {
+		System.out.println("I am Learing Kafka");
+		 Properties props = new Properties();
+		 props.put("bootstrap.servers", "localhost:9092");
+		 props.put("acks", "all");
+		 props.put("retries", 0);
+		 props.put("batch.size", 16384);
+		 props.put("linger.ms", 1);
+		 props.put("buffer.memory", 33554432);
+		 props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		 Producer<String, String> producer = new KafkaProducer<>(props);
+		 for(int i = 0; i < 100; i++){
+			 System.out.println("Sending to Kafka topic");
+		     producer.send(new ProducerRecord<String, String>("test", "Sending to Kafka topic::"+Integer.toString(i), Integer.toString(i)));
+		 }
+		 producer.close();
+	}
+}
