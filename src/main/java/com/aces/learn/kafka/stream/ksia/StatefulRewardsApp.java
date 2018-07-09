10ebaaf4b2e88ebc57d908be795f21046769cbc8
Ajay Agarwal <ajay.agarwal@resonate.com>
2018-07-09 14:01:52 -0400

    Joiner Processor

--- src/main/java/com/aces/learn/kafka/stream/ksia/StatefulRewardsApp.java
+++ src/main/java/com/aces/learn/kafka/stream/ksia/StatefulRewardsApp.java
@@ -103,9 +103,9 @@ public class StatefulRewardsApp {
      */
     private static Properties getProperties() {
         Properties props = new Properties();
-        props.put(StreamsConfig.CLIENT_ID_CONFIG, "FirstZmart-Kafka-Streams-Client");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "zmart-purchases");
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "FirstZmart-Kafka-Streams-App");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "StatefulRewardsApp");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "StatefulRewardsApp");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "StatefulRewardsApp");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
         props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
